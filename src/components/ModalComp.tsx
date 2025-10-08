80e21b44bd7a8fec4e7bb9aee3a07b2262cf1525
pishlo <soni.kozaliev@gmail.com>
2025-10-08 22:02:22 +0200

    Updated modal emoji logic

--- src/components/ModalComp.tsx
+++ src/components/ModalComp.tsx
@@ -7,23 +7,36 @@ export type TModalProps = {
     moves: number
 }
 
-const ModalComp = ({ showModal, toggleModal, handleRestart, moves}: TModalProps) => {
+const ModalComp = ({ showModal, toggleModal, handleRestart, moves }: TModalProps) => {
+
+    const getEmoji = (moves: number) => {
+        if (moves <= 10) return "🤯"       
+        if (moves <= 16) return "😎"       
+        if (moves <= 24) return "😅"       
+        return "💀"
+    }
+
+    const emoji = getEmoji(moves)
+
     return (
         <section
             className={styles.final_result}
-            style={{ visibility: showModal ? "visible" : "hidden"}}
+            style={{ visibility: showModal ? "visible" : "hidden" }}
         >
-
             <button onClick={() => toggleModal(false)} className={styles.final_btn}>X</button>
+
             <div className={styles.final_container}>
                 <h2>Final Score</h2>
                 <span className={styles.score}>{moves} moves</span>
-                <span className={styles.final_icon + " final_icon animate_delay-1s"}>
-                    🏆
+
+                <span className={`${styles.final_icon} final_icon animate_delay-1s`}>
+                    {emoji}
                 </span>
-                <span onClick={handleRestart} className={styles.final_text}>Click to play again</span>
+
+                <span onClick={handleRestart} className={styles.final_text}>
+                    Click to play again
+                </span>
             </div>
-        
         </section>
     )
 }
